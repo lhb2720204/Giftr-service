2c2cf90ce0d281fb54370f6bcd886bf038ea6b8c
Hongbo Li <lihongbo1991@gmail.com>
2016-05-30 16:09:12 -0700

    Add advanced product search

--- src/main/java/com/akhahaha/giftr/service/controllers/ProductsController.java
+++ src/main/java/com/akhahaha/giftr/service/controllers/ProductsController.java
@@ -2,14 +2,19 @@ package com.akhahaha.giftr.service.controllers;
 
 import com.akhahaha.giftr.service.data.dao.DAOManager;
 import com.akhahaha.giftr.service.data.dao.ProductDAO;
+import com.akhahaha.giftr.service.data.dao.queryBuilder.ProductQueryBuilder;
+import com.akhahaha.giftr.service.data.models.Gender;
+import com.akhahaha.giftr.service.data.models.GiftType;
 import com.akhahaha.giftr.service.data.models.Product;
+import com.akhahaha.giftr.service.data.models.UserStatus;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.List;
+import java.util.List;	
 
 /**
  * Products service controller
@@ -23,10 +28,17 @@ public class ProductsController {
 
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<?> searchProducts(
-            @RequestParam(required = false) String keyword) {
-        // TODO Validate credentials
-        // TODO Add more search fields
-        List<Product> products = productDAO.searchProductsByKeyword(keyword);
+            @RequestParam(required = false) String keyword,
+            @RequestParam(required = false) Integer start,
+            @RequestParam(required = false) Integer numResults,
+            @RequestParam(required = false) Integer minPrice,
+            @RequestParam(required = false) Integer maxPrice,
+            @RequestParam(required = false) String sort) {
+    	
+    	ProductQueryBuilder productQueryBuilder = new ProductQueryBuilder();
+    	setProductQueryFields(productQueryBuilder, keyword, start, numResults, minPrice, maxPrice, sort);
+    	
+        List<Product> products = productDAO.searchProductsByAdvancedSearch(productQueryBuilder);
 
         HttpHeaders headers = new HttpHeaders();
         headers.setLocation(ServletUriComponentsBuilder
@@ -34,4 +46,15 @@ public class ProductsController {
                 .buildAndExpand().toUri());
         return new ResponseEntity<>(products, headers, HttpStatus.OK);
     }
+    
+    private void setProductQueryFields(ProductQueryBuilder productQueryBuilder,
+    		String keyword, Integer start, Integer numResults, Integer minPrice, Integer maxPrice, String sort) {
+        if (keyword != null) productQueryBuilder.setKeyword(keyword);
+        if (start != null) productQueryBuilder.setStart(start);
+        if (numResults != null) productQueryBuilder.setNumResults(numResults);
+        if (minPrice != null) productQueryBuilder.setMinPrice(minPrice);
+        if (maxPrice != null) productQueryBuilder.setMaxPrice(maxPrice);
+        if (sort != null) productQueryBuilder.setSort(sort);
+    }
+    
 }
